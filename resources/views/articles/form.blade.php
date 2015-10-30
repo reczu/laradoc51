3cd48ae442e45f52bc92f6730970854d08b38447
reczu <reczek.adam@gmail.com>
2015-10-30 15:45:30 +0100

    lesson 24

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -17,10 +17,19 @@
 <!-- Tags Form Input -->
 <div class="form-group">
     {!! Form::label('tag_list', 'Tags:') !!}
-    {!! Form::select('tag_list[]', $tags, null, ['class' => 'form-control', 'multiple']) !!}
+    {!! Form::select('tag_list[]', $tags, null, ['id' => 'tag_list', 'class' => 'form-control', 'multiple']) !!}
 </div>
 
 <!-- Add article Form Input-->
 <div class="form-group">
     {!! Form::submit($submitButton, ['class' => 'btn btn-primary form-control']) !!}
 </div>
+
+
+@section('footer')
+    <script>
+        $('#tag_list').select2({
+            'placeholder': 'Choos a tag'
+        });
+    </script>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" />
+
 </head>
 <body>
     @include('partials.navbar')
@@ -17,6 +17,10 @@
     <script>
         //$('div.alert').not('.alert-important').delay(3000).slideUp(300);
     </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
     @yield('footer')
 
 </body>
